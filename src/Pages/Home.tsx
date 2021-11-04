c24144b27a60e9f18b459f2b37143386da607154
galaxy126 <galaxy85126@gmail.com>
2021-11-05 02:44:15 +0800

    fix: ui

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -128,20 +128,25 @@ const Home = () => {
 								<svg width="11" fill="#888" viewBox="0 0 11 11"><path d="M6.431 5.25L2.166 9.581l.918.919 5.25-5.25L3.084 0l-.918.919L6.43 5.25z"></path></svg>
 							</div>
 						</label>
+						
+						
 						<div className="list">
-							<ul>
-								<li className="disabled search">
-									<svg width="24" height="24" fill="#5e6673" viewBox="0 0 24 24"><path d="M3 10.982c0 3.845 3.137 6.982 6.982 6.982 1.518 0 3.036-.506 4.149-1.416L18.583 21 20 19.583l-4.452-4.452c.81-1.113 1.416-2.631 1.416-4.149 0-1.922-.81-3.643-2.023-4.958C13.726 4.81 11.905 4 9.982 4 6.137 4 3 7.137 3 10.982zM13.423 7.44a4.819 4.819 0 011.416 3.441c0 1.315-.506 2.53-1.416 3.44a4.819 4.819 0 01-3.44 1.417 4.819 4.819 0 01-3.441-1.417c-1.012-.81-1.518-2.023-1.518-3.339 0-1.315.506-2.53 1.416-3.44.911-1.012 2.227-1.518 3.542-1.518 1.316 0 2.53.506 3.44 1.416z"></path></svg>
-									<input type="text" value={status.query} maxLength={6} onChange={(e)=>onChangeQuery(e.target.value.trim())} />
-								</li>
-								{tokens.map(v=>(
-									<li key={v.icon} onClick={()=>onChangeToken(v.icon)}>
-										<img src={`/coins/${v.icon}.svg`} loading='lazy' style={{width:20, height:20, marginRight:10}} alt={v.icon} />
-										<span>{v.coin}</span>
-										<small>{v.label}</small>
-									</li>
-								))}
-							</ul>
+							<div className="search">
+								<svg width="24" height="24" fill="#5e6673" viewBox="0 0 24 24"><path d="M3 10.982c0 3.845 3.137 6.982 6.982 6.982 1.518 0 3.036-.506 4.149-1.416L18.583 21 20 19.583l-4.452-4.452c.81-1.113 1.416-2.631 1.416-4.149 0-1.922-.81-3.643-2.023-4.958C13.726 4.81 11.905 4 9.982 4 6.137 4 3 7.137 3 10.982zM13.423 7.44a4.819 4.819 0 011.416 3.441c0 1.315-.506 2.53-1.416 3.44a4.819 4.819 0 01-3.44 1.417 4.819 4.819 0 01-3.441-1.417c-1.012-.81-1.518-2.023-1.518-3.339 0-1.315.506-2.53 1.416-3.44.911-1.012 2.227-1.518 3.542-1.518 1.316 0 2.53.506 3.44 1.416z"></path></svg>
+								<input type="text" value={status.query} maxLength={6} onChange={(e)=>onChangeQuery(e.target.value.trim())} />
+							</div>
+							<div style={{overflowY:'auto', maxHeight:200}}>
+								<ul>
+									{tokens.map(v=>(
+										<li key={v.icon} onClick={()=>onChangeToken(v.icon)}>
+											<img src={`/coins/${v.icon}.svg`} loading='lazy' style={{width:20, height:20, marginRight:10}} alt={v.icon} />
+											<span>{v.coin}</span>
+											<small>{v.label}</small>
+										</li>
+									))}
+								</ul>
+							</div>
+							
 						</div>
 						
 						<label className="overlay" htmlFor="asset"></label>
